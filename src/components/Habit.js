b420b5566f435ffac9f4e227055bd569a5299e52
paaaarv <pkumar@trinity.edu>
2019-03-25 13:16:48 -0500

    added checkboxes for each day of the week

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import WeekTracker from './WeekTracker.js'
 
 const Habit = props => {
   return(
   <div>
-  <h4>{props.name} </h4> 
+  <h4>{props.name} </h4>
   <p> IDEAL: {props.frequency} per {props.dayOrWeek}</p>
+  <WeekTracker/> 
+
   </div>
 )
 
--- src/components/WeekTracker.js
+++ src/components/WeekTracker.js
@@ -1,9 +1,51 @@
 import React from 'react'
 
 
+const Checkbox = (name,  onChange) => (
+  <div>
+    <input type="checkbox" name={name} value={name} onChange={onChange}/>
+  </div>
+)
+
 
 export default class WeekTracker extends React.Component{
+  constructor(props){
+    super(props)
+
+  }
+
+  handleChange = () =>{
+
+  }
+  render() {
+    return(
+      <div>
+        <label>
+          mon.
+        <Checkbox name="monday"  onChange={this.handleChange()}/>
+        </label>
+        <label>
+          tues.
+        <Checkbox name="monday"  onChange={this.handleChange()}/>
+        </label> <label>
+          wed.
+        <Checkbox name="monday"  onChange={this.handleChange()}/>
+        </label> <label>
+          thurs.
+        <Checkbox name="monday"  onChange={this.handleChange()}/>
+        </label> <label>
+          fri.
+        <Checkbox name="monday"  onChange={this.handleChange()}/>
+        </label> <label>
+          sat.
+        <Checkbox name="monday"  onChange={this.handleChange()}/>
+        </label> <label>
+          sun.
+        <Checkbox name="monday"  onChange={this.handleChange()}/>
+        </label>
 
 
-
+      </div>
+    )
+  }
 }
